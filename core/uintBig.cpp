1049b9a0207d30c8e1c2246a4c7b420f230e4499
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:29:27 +0100

    Update uintBig.cpp
    Grimm to MWG

--- core/uintBig.cpp
+++ core/uintBig.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +15,7 @@
 
 #include "uintBig.h"
 
-namespace grimm {
+namespace MWG {
 
 
 	char ChFromHex(uint8_t v)
@@ -373,4 +374,4 @@ namespace grimm {
 #endif // NDEBUG
 	}
 
-} // namespace grimm
+} // namespace MWG
