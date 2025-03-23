09cb66073946781afb2d29e6fb6c537cb7f0d797
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:03:07 +0100

    Update WalletAddressDTO.java
    Grimm to MWG

--- android/com/mw/grimm/grimmwallet/core/entities/dto/WalletAddressDTO.java
+++ android/com/mw/grimm/grimmwallet/core/entities/dto/WalletAddressDTO.java
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.mw.grimm.grimmwallet.core.entities.dto;
+package com.mw.MWG.MWGwallet.core.entities.dto;
 
 import java.util.*; 
 
