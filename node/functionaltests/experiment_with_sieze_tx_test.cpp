4dddcafa317d546966247c68c6c77d8b7ca41e4d
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:12:34 +0100

    Update experiment_with_sieze_tx_test.cpp

--- node/functionaltests/experiment_with_sieze_tx_test.cpp
+++ node/functionaltests/experiment_with_sieze_tx_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include "tools/tx_generator.h"
 #include "../core/serialization_adapters.h"
 
-using namespace grimm;
+using namespace MWG;
 using namespace ECC;
 
 int main(int argc, char* argv[])
