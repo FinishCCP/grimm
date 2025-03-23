88c06f92d8c3fef98b033d5f359cfb7ad03d257c
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:32:17 +0100

    Update server_stub.cpp

--- pow/unittests/server_stub.cpp
+++ pow/unittests/server_stub.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 #include <future>
 #include <boost/filesystem.hpp>
 
-using namespace grimm;
+using namespace MWG;
 
 std::unique_ptr<IExternalPOW> server;
 int idInt = 0;
