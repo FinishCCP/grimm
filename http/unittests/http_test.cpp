6188491cfccf57d226bd70b3e0ebd0e54dd7c230
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:36:13 +0100

    Update http_test.cpp
    Grimm to MWG

--- http/unittests/http_test.cpp
+++ http/unittests/http_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 #include "utility/helpers.h"
 #include "utility/logger.h"
 
-using namespace grimm;
+using namespace MWG;
 
 namespace {
 
