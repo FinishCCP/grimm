b921af6c11125fec6421e2cd802e10d7d1e21218
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:38:56 +0100

    Update http_msg_creator.h
    Grimm to MWG

--- http/http_msg_creator.h
+++ http/http_msg_creator.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "utility/io/fragment_writer.h"
 #include "nlohmann/json_fwd.hpp"
 
-namespace grimm {
+namespace MWG {
 
 struct HeaderPair {
     const char* head;
