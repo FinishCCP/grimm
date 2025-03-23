ff9c540c22c99e65cab157632f863461fffad86d
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:32:52 +0100

    Update server.cpp
    Grimm to MWG

--- explorer/server.cpp
+++ explorer/server.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 #include <boost/algorithm/string/trim.hpp>
 #include <fstream>
 
-namespace grimm { namespace explorer {
+namespace MWG { namespace explorer {
 
 namespace {
 
