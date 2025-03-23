f636a52af2bb38cd06ab8bd7b5c7e5562359b006
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:35:50 +0100

    Update http_parser_test.cpp
    Grimm to MWG

--- http/unittests/http_parser_test.cpp
+++ http/unittests/http_parser_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "utility/helpers.h"
 #include "utility/logger.h"
 
-using namespace grimm;
+using namespace MWG;
 using namespace std;
 
 namespace {
