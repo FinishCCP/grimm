23d69e39cf2f110f10b4e34a330a27e277046887
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:35:07 +0100

    Update stratum.cpp

--- pow/stratum.cpp
+++ pow/stratum.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 
 using json = nlohmann::json;
 
-namespace grimm::stratum {
+namespace MWG::stratum {
 
 static const std::string method_strings[] = {
 #define METHOD_STR(_, M, __) std::string(#M),
