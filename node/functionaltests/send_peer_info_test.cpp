5c6903c60eb0be278e2a036c6eb9fef660a20177
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:15:36 +0100

    Update send_peer_info_test.cpp

--- node/functionaltests/send_peer_info_test.cpp
+++ node/functionaltests/send_peer_info_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 #include <thread>
 #include <future>
 
-using namespace grimm;
+using namespace MWG;
 using namespace ECC;
 
 class TestNodeConnection : public BaseTestNode
