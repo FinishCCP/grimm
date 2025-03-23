791d30245c97eb5ebd128145ba100cb06191e695
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:30:00 +0100

    Update msg_serializer.cpp

--- p2p/msg_serializer.cpp
+++ p2p/msg_serializer.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +16,7 @@
 #include "msg_serializer.h"
 #include <assert.h>
 
-namespace grimm {
+namespace MWG {
 
 MsgSerializeOstream::MsgSerializeOstream(size_t fragmentSize, MsgHeader defaultHeader) :
     _writer(
