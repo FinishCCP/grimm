c18279e3b5213316da49ea80c848938058a15fd4
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:31:19 +0100

    Update adapter.cpp
    Grimm to MWG

--- explorer/adapter.cpp
+++ explorer/adapter.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,7 +22,7 @@
 #include "utility/helpers.h"
 #include "utility/logger.h"
 
-namespace grimm { namespace explorer {
+namespace MWG { namespace explorer {
 
 namespace {
 
