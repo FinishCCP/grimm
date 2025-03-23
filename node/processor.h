8fb72091e53c25e9c0ca566d687a6297048339cf
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:25:45 +0100

    Update processor.h

--- node/processor.h
+++ node/processor.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 #include "db.h"
 #include "txpool.h"
 
-namespace grimm {
+namespace MWG {
 
 class NodeProcessor
 {
@@ -352,4 +353,4 @@ struct LogSid
 std::ostream& operator << (std::ostream& s, const LogSid&);
 
 
-} // namespace grimm
+} // namespace MWG
