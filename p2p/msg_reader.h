6dd351dd90994d7804dc941ac8a47069ac75d503
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:29:33 +0100

    Update msg_reader.h

--- p2p/msg_reader.h
+++ p2p/msg_reader.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include <vector>
 #include <bitset>
 
-namespace grimm {
+namespace MWG {
 
 /// Extracts (serialized, raw data) individual messages from stream, performs header/size validation
 class MsgReader {
