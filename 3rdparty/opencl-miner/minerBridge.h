fb906fcb988dc9b5748f94d3b7210b62d0d2285b
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 19:32:28 +0100

    Update minerBridge.h
    Grimm to MWG

--- 3rdparty/opencl-miner/minerBridge.h
+++ 3rdparty/opencl-miner/minerBridge.h
@@ -1,12 +1,13 @@
-// GRIMM OpenCL Miner
+// MWG OpenCL Miner
 // Miner<->stratum bridge class
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
 // Copyright 2018 Wilke Trei
+// Copyright 2025 MWG Team
 #pragma once
 #include <vector>
 #include <stdint.h>
 
-namespace grimmMiner {
+namespace MWGMiner {
 
 class minerBridge {
 public:
