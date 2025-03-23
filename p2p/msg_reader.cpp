5229908c15c41fc3d137332d25820c15d5c929c1
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:29:15 +0100

    Update msg_reader.cpp

--- p2p/msg_reader.cpp
+++ p2p/msg_reader.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include <assert.h>
 #include <algorithm>
 
-namespace grimm {
+namespace MWG {
 
 MsgReader::MsgReader(ProtocolBase& protocol, uint64_t streamId, size_t defaultSize) :
     _protocol(protocol),
