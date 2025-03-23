708a90f964de3e227b6a0a71901825b7ab5f1013
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:41:48 +0100

    Update mnemonic.h
    Grimm to MWG

--- mnemonic/mnemonic.h
+++ mnemonic/mnemonic.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 #include <vector>
 #include <string>
 
-namespace grimm
+namespace MWG
 {
     class MnemonicException : public std::runtime_error {
     public:
