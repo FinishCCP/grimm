8895f8a046a0fad51e86ad10a4ed0a5809bc7457
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 19:27:32 +0100

    Update clMinerLib.h
    Grimm to MWG

--- 3rdparty/opencl-miner/clMinerLib.h
+++ 3rdparty/opencl-miner/clMinerLib.h
@@ -1,7 +1,8 @@
-// GRIMM OpenCL Miner
+// MWG OpenCL Miner
 // Miner library interface
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
 // Copyright 2018 Wilke Trei
+// Copyright 2025 MWG Team
 #pragma once
 #include <vector>
 #include <stdint.h>
