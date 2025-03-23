84b20f6f3295cea7abab87cb30fa221d6ce2af65
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:41:21 +0100

    Update qr.cpp

--- ui/model/qr.cpp
+++ ui/model/qr.cpp
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -71,7 +72,7 @@ void QR::update()
     }
     
     QUrl url;
-    url.setScheme("grimm");
+    url.setScheme("MWG");
     url.setPath(m_addr);
     url.setQuery(query);
 
