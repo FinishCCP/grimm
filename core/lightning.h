915927f0351a4a99687f19d64711af20a59ff0e5
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 20:21:42 +0100

    Update lightning.h
    Grimm to MWG

--- core/lightning.h
+++ core/lightning.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include "negotiator.h"
 #include "fly_client.h"
 
-namespace grimm {
+namespace MWG {
 namespace Lightning {
 
 	using namespace Negotiator;
@@ -215,4 +216,4 @@ namespace Lightning {
 
 
 } // namespace Lightning
-} // namespace grimm
+} // namespace MWG
