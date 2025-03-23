cbc14bb5a441c3dddea79557a3e92da959a37067
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:30:37 +0100

    Update protocol.h

--- p2p/protocol.h
+++ p2p/protocol.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include "msg_serializer.h"
 #include <stdexcept>
 
-namespace grimm {
+namespace MWG {
 
 /// Network<=>App logic message-oriented protocol
 class Protocol : public ProtocolBase {
