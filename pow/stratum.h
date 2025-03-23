39f701edef9645bdf180814fbf29f15e376d2372
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-23 21:35:25 +0100

    Update stratum.h

--- pow/stratum.h
+++ pow/stratum.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include "utility/io/fragment_writer.h"
 #include <string>
 
-namespace grimm::stratum {
+namespace MWG::stratum {
 
 #define STRATUM_METHODS(macro) \
     macro(0, null_method, Dummy) \
