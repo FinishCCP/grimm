6948007ebf6f7c35bd7096d8c0cc9b90396289fb
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:21:57 +0100

    Update bitcoind016.cpp

--- wallet/bitcoin/bitcoind016.cpp
+++ wallet/bitcoin/bitcoind016.cpp
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 
 using json = nlohmann::json;
 
-namespace grimm
+namespace MWG
 {    
     namespace
     {
