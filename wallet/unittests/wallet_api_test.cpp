6e99d3a48403edf61af23be7de5308c4e1e456d9
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:37:48 +0100

    Update wallet_api_test.cpp

--- wallet/unittests/wallet_api_test.cpp
+++ wallet/unittests/wallet_api_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,8 +21,8 @@
 #include "nlohmann/json.hpp"
 
 using namespace std;
-using namespace grimm;
-using namespace grimm::wallet;
+using namespace MWG;
+using namespace MWG::wallet;
 using json = nlohmann::json;
 
 WALLET_TEST_INIT
