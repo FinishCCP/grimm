0daa51dd75c310b22baff5e7a1afcdedd3163e7b
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:20:44 +0100

    Update bitcoin_side.cpp

--- wallet/bitcoin/bitcoin_side.cpp
+++ wallet/bitcoin/bitcoin_side.cpp
@@ -1,4 +1,6 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
+
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -63,12 +65,12 @@ namespace
     }
 }
 
-namespace grimm::wallet
+namespace MWG::wallet
 {
-    BitcoinSide::BitcoinSide(BaseTransaction& tx, IBitcoinBridge::Ptr bitcoinBridge, bool isGrimmSide)
+    BitcoinSide::BitcoinSide(BaseTransaction& tx, IBitcoinBridge::Ptr bitcoinBridge, bool isMWGSide)
         : m_tx(tx)
         , m_bitcoinBridge(bitcoinBridge)
-        , m_isBtcOwner(!isGrimmSide)
+        , m_isBtcOwner(!isMWGSide)
     {
     }
 
@@ -209,7 +211,7 @@ namespace grimm::wallet
     {
         NoLeak<uintBig> secretPrivateKey;
         GenRandom(secretPrivateKey.V);
-        m_tx.SetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, false, GRIMM_REDEEM_TX);
+        m_tx.SetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, false, MWG_REDEEM_TX);
     }
 
     libbitcoin::chain::script BitcoinSide::CreateAtomicSwapContract()
@@ -221,7 +223,7 @@ namespace grimm::wallet
 
         libbitcoin::wallet::ec_public secretPublicKey;
 
-        if (NoLeak<uintBig> secretPrivateKey; m_tx.GetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, SubTxIndex::GRIMM_REDEEM_TX))
+        if (NoLeak<uintBig> secretPrivateKey; m_tx.GetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, SubTxIndex::MWG_REDEEM_TX))
         {
             // secretPrivateKey -> secretPublicKey
             libbitcoin::ec_secret secret;
@@ -232,7 +234,7 @@ namespace grimm::wallet
         }
         else
         {
-            Point publicKeyPoint = m_tx.GetMandatoryParameter<Point>(TxParameterID::AtomicSwapSecretPublicKey, SubTxIndex::GRIMM_REDEEM_TX);
+            Point publicKeyPoint = m_tx.GetMandatoryParameter<Point>(TxParameterID::AtomicSwapSecretPublicKey, SubTxIndex::MWG_REDEEM_TX);
 
             // publicKeyPoint -> secretPublicKey
             auto publicKeyRaw = SerializePubkey(ConvertPointToPubkey(publicKeyPoint));
@@ -577,7 +579,7 @@ namespace grimm::wallet
             {
                 // AtomicSwapSecretPrivateKey -> libbitcoin::wallet::ec_private
                 NoLeak<uintBig> secretPrivateKey;
-                m_tx.GetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, SubTxIndex::GRIMM_REDEEM_TX);
+                m_tx.GetParameter(TxParameterID::AtomicSwapSecretPrivateKey, secretPrivateKey.V, SubTxIndex::MWG_REDEEM_TX);
                 libbitcoin::ec_secret secret;
                 std::copy(std::begin(secretPrivateKey.V.m_pData), std::end(secretPrivateKey.V.m_pData), secret.begin());
 
