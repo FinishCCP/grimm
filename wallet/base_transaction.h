1db1c70524f391f6478acc099bff38cb7885bade
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 15:04:29 +0100

    Update base_transaction.h

--- wallet/base_transaction.h
+++ wallet/base_transaction.h
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +26,7 @@
 
 
 
-namespace grimm::wallet
+namespace MWG::wallet
 {
     TxID GenerateTxID();
 
