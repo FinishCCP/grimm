823a859fa8537a2867c660e0ea81512953d17b05
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:40:50 +0100

    Update json_serializer.h

--- utility/io/json_serializer.h
+++ utility/io/json_serializer.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "utility/io/fragment_writer.h"
 #include "nlohmann/json_fwd.hpp"
 
-namespace grimm {
+namespace MWG {
 
 // appends json msg to out by fragment writer
 bool serialize_json_msg(io::FragmentWriter& packer, const nlohmann::json& o);
