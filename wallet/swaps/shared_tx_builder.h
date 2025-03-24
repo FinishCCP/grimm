caeef78438803bb8ef6407138cd7eb726efdb802
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:28:22 +0100

    Update shared_tx_builder.h

--- wallet/swaps/shared_tx_builder.h
+++ wallet/swaps/shared_tx_builder.h
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 
 #include "common.h"
 
-namespace grimm::wallet
+namespace MWG::wallet
 {
     class SharedTxBuilder : public BaseTxBuilder
     {
