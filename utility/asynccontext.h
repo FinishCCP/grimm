5f30fba0501b172b669098d4e15743e38a3a6258
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:52:00 +0100

    Update asynccontext.h

--- utility/asynccontext.h
+++ utility/asynccontext.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 #include <unordered_map>
 #include <atomic>
 
-namespace grimm {
+namespace MWG {
 
 class AsyncContext;
 
