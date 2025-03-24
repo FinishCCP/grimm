36d212ef95d78bd4737c5da413fd182711991189
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:46:33 +0100

    Update wallet_test_environment.cpp

--- wallet/unittests/wallet_test_environment.cpp
+++ wallet/unittests/wallet_test_environment.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,8 +15,8 @@
 
 #include "http/http_client.h"
 
-using namespace grimm;
-using namespace grimm::wallet;
+using namespace MWG;
+using namespace MWG::wallet;
 using namespace std;
 using namespace ECC;
 using json = nlohmann::json;
