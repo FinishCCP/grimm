489ad6f295647575be100512410cafb6e034277b
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:34:33 +0100

    Update asyncevent.cpp

--- utility/io/asyncevent.cpp
+++ utility/io/asyncevent.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +16,7 @@
 #include "asyncevent.h"
 #include <assert.h>
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 AsyncEvent::Ptr AsyncEvent::create(Reactor& reactor, AsyncEvent::Callback&& callback) {
     assert(callback);
