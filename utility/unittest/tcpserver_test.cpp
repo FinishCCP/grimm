04beb7144ccfbf8e8ea220806c8e65010efe182a
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:50:28 +0100

    Update tcpserver_test.cpp

--- utility/unittest/tcpserver_test.cpp
+++ utility/unittest/tcpserver_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,8 +22,8 @@
 #endif
 #include "utility/logger.h"
 
-using namespace grimm;
-using namespace grimm::io;
+using namespace MWG;
+using namespace MWG::io;
 using namespace std;
 
 Reactor::Ptr reactor;
