d5402532866613578e87508f394440c5aa10d14a
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:41:26 +0100

    Update mempool.h

--- utility/io/mempool.h
+++ utility/io/mempool.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 template <class T, size_t DATA_SIZE> class MemPool {
 public:
