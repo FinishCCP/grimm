9384202e466b9eab5ba85dce3ef926b526b584e9
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:37:44 +0100

    Update coarsetimer.h

--- utility/io/coarsetimer.h
+++ utility/io/coarsetimer.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+//Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 #include <vector>
 #include <limits>
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 /// Coarse timer helper, for connect/reconnect timers
 class CoarseTimer {
