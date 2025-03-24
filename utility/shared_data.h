221d114c07ad3c883dd7c79a86bba23f2b956b81
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:59:15 +0100

    Update shared_data.h

--- utility/shared_data.h
+++ utility/shared_data.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +16,7 @@
 #pragma once
 #include <shared_mutex>
 
-namespace grimm {
+namespace MWG {
 
 template <class Data> class SharedData {
     mutable std::shared_mutex _rwLock;
