8d140b843dbba3a59107695e4a012af784b5c8b6
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:26:02 +0100

    Update options.h

--- wallet/litecoin/options.h
+++ wallet/litecoin/options.h
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 #include "../bitcoin/options.h"
 
-namespace grimm
+namespace MWG
 {
     using LitecoinOptions = BitcoinOptions;
 }
