5a029ccfd60563f44711ce9fd4a8b77dd148a172
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:55:59 +0100

    Update log_rotation.h

--- utility/log_rotation.h
+++ utility/log_rotation.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include "io/timer.h"
 #include <map>
 
-namespace grimm {
+namespace MWG {
 
 /// Rotates logs by timer and cleans old files
 class LogRotation {
