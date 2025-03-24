95bab375f87e4cc3fd1572c2f608931de7acd8c0
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:57:28 +0100

    Update message_queue.h

--- utility/message_queue.h
+++ utility/message_queue.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 #include <deque>
 #include <assert.h>
 
-namespace grimm {
+namespace MWG {
 
 /// Inter-thread message queue, backend for RX and TX sides (see below)
 /// Current impl (may be changed if performance bottleneck detected):
