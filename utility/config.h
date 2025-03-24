d612024ebafd947ab858645195bf6375661eae65
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:53:39 +0100

    Update config.h

--- utility/config.h
+++ utility/config.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,7 +29,7 @@
     using boost::any_cast;
 #endif
 
-namespace grimm {
+namespace MWG {
 
 class Config;
 
