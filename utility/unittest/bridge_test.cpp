380a9eb32a417ca769f93b042c88fcc16e3e8bc0
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:47:15 +0100

    Update bridge_test.cpp

--- utility/unittest/bridge_test.cpp
+++ utility/unittest/bridge_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,7 +23,7 @@
 
 
 using namespace std;
-using namespace grimm;
+using namespace MWG;
 
 // Some requests to be forwarded to another thread
 struct Req1 { int x=0; };
