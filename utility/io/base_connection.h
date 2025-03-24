2743e8e260ca78c9d049c2d72f2aa0d52154a64d
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:35:13 +0100

    Update base_connection.h

--- utility/io/base_connection.h
+++ utility/io/base_connection.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +16,7 @@
 #pragma once
 #include "utility/io/tcpstream.h"
 
-namespace grimm {
+namespace MWG {
 
 /// Reads-writes messages from-to connected stream
 class BaseConnection {
