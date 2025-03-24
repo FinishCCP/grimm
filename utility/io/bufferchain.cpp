28ac364a4eafa4f64a911b19f78f609adf47aa47
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:39:16 +0100

    Update bufferchain.cpp

--- utility/io/bufferchain.cpp
+++ utility/io/bufferchain.cpp
@@ -1,5 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
-//Copyright 2019 The MWG Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
