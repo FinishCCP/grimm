4b22b68786ba795bc0e719fc78f20f8e2e6793ea
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:48:05 +0100

    Update logger_test.cpp

--- utility/unittest/logger_test.cpp
+++ utility/unittest/logger_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@
 #include <thread>
 #include "wallet/secstring.h"
 
-using namespace grimm;
+using namespace MWG;
 
 struct XXX {
     int z = 333;
