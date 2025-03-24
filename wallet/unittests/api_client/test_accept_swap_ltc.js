6f4eb1bcd14b078c3bc7d58da3b06d6615fea71f
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:30:35 +0100

    Update test_accept_swap_ltc.js

--- wallet/unittests/api_client/test_accept_swap_ltc.js
+++ wallet/unittests/api_client/test_accept_swap_ltc.js
@@ -13,7 +13,7 @@ client.connect(10010, '127.0.0.1', function () {
 			    "amount": 1000,
 			    "swapAmount": 90000,
                 "swapCoin": "ltc",
-			    "grimmSide": false,
+			    "MWGSide": false,
 			}
 		}) + '\n');
 });
