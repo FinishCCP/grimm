91758e0ccc0431c82d87adb4b0a6a69ccb51c5a8
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:57:47 +0100

    Update serialize.h

--- utility/serialize.h
+++ utility/serialize.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -33,7 +34,7 @@
 #pragma warning(pop)
 #endif
 
-namespace grimm {
+namespace MWG {
 
 /// Yas lib options
 constexpr int SERIALIZE_OPTIONS = yas::binary | yas::no_header | yas::elittle | yas::compacted;
