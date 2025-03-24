6e52fc79c2f42966055e85adc98e33617837876f
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:42:25 +0100

    Update sslio.cpp

--- utility/io/sslio.cpp
+++ utility/io/sslio.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 #define LOG_DEBUG_ENABLED 1
 #include "utility/logger.h"
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 namespace {
 
