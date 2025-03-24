0f8c2278d7d8b86ceffe4f5b47222ae19a1ba9fb
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:49:12 +0100

    Update shared_data_test.cpp

--- utility/unittest/shared_data_test.cpp
+++ utility/unittest/shared_data_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +20,7 @@
 #include <vector>
 #include <assert.h>
 
-using namespace grimm;
+using namespace MWG;
 
 class Barrier {
 private:
