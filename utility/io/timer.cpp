617a5db7bd8c63fd425f6fe111d807b8110735e3
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:45:49 +0100

    Update timer.cpp

--- utility/io/timer.cpp
+++ utility/io/timer.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 #endif
 #include "utility/logger.h"
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 Timer::Ptr Timer::create(Reactor& reactor) {
     Ptr timer(new Timer());
