3cf1a79f538f952c6b763ecc347fe522fdbf7c25
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:33:48 +0100

    Update test_start_swap_ltc.js

--- wallet/unittests/api_client/test_start_swap_ltc.js
+++ wallet/unittests/api_client/test_start_swap_ltc.js
@@ -14,8 +14,8 @@ client.connect(10000, '127.0.0.1', function () {
 			    "fee": 10,
 			    "swapAmount": 90000,
 			    "swapCoin": "ltc",
-			    "grimmSide": true,
-			    "address": "290146b2d32d2c83690ceeb8f2da41fb892a61130e01f298b216424d39552690431"
+			    "MWGSide": true,
+			    "address": "290146b2d32d2c83690ceeb8f2da41fb892a61130e01f298b216424d39552690431" check shavad
 			}
 		}) + '\n');
 });
