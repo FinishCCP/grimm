0741d7ad13e39837704c0541d363b1685ae88499
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:58:58 +0100

    Update serialize_streams.h

--- utility/serialize_streams.h
+++ utility/serialize_streams.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 #include <string.h>
 #include <vector>
 
-namespace grimm { namespace detail {
+namespace MWG { namespace detail {
 
 // Growing buffer serializer ostream
 struct SerializeOstream {
