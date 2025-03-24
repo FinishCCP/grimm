786e53b3cee809b531ba48cb835c6c187d6cd38c
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:45:16 +0100

    Update tcpstream.cpp

--- utility/io/tcpstream.cpp
+++ utility/io/tcpstream.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@
 #define LOG_DEBUG_ENABLED 0
 #include "utility/logger.h"
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 TcpStream::TcpStream() :
     _onDataWritten(BIND_THIS_MEMFN(on_data_written))
