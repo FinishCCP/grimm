4465d642a9e422f03997def10265905cc5a78a53
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:43:03 +0100

    Update sslserver.cpp

--- utility/io/sslserver.cpp
+++ utility/io/sslserver.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "sslstream.h"
 #include <assert.h>
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 TcpServer::Ptr SslServer::create(
     Reactor& reactor, Address bindAddress, Callback&& callback,
