c5c0451af0cae42082060825c379bd109c5a2158
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:40:15 +0100

    Update fragment_writer.h

--- utility/io/fragment_writer.h
+++ utility/io/fragment_writer.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "buffer.h"
 #include <functional>
 
-namespace grimm { namespace io {
+namespace MWG { namespace io {
 
 /// For long messages, it collects raw bytes of unknown size (typically from serializer)
 /// and arranges them into fixed-length shared fragments to be forwarded to network.
