d2aa725d401bb93d60d10b4fcce2dc47e2b6942b
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:52:16 +0100

    Update bridge.h

--- utility/bridge.h
+++ utility/bridge.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 #include "utility/message_queue.h"
 #include "utility/helpers.h"
 
-namespace grimm {
+namespace MWG {
 
 /// Inter-thread bridge template
 template <typename Interface> class Bridge : public Interface {
