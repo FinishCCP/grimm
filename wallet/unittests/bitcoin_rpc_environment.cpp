b768e1de79fe34552f452f59b73a7dd2b3cc2571
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:35:08 +0100

    Update bitcoin_rpc_environment.cpp

--- wallet/unittests/bitcoin_rpc_environment.cpp
+++ wallet/unittests/bitcoin_rpc_environment.cpp
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,7 +27,7 @@ const uint16_t PORT = 13300;
 const std::string btcUserName = "Alice";
 const std::string btcPass = "123";
 
-using namespace grimm;
+using namespace MWG;
 using json = nlohmann::json;
 
 namespace
