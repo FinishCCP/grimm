4edd2f8272833f00635b1720c8b30a776a37affa
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 15:17:39 +0100

    Update wallet_transaction.h

--- wallet/wallet_transaction.h
+++ wallet/wallet_transaction.h
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2025 MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,7 +23,7 @@
 #include <boost/optional.hpp>
 #include "utility/logger.h"
 
-namespace grimm::wallet
+namespace MWG::wallet
 {
     class BaseTxBuilder;
 
