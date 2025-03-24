bb91e17d6af38e8345255c6298dbe0c24aa4831b
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 08:47:33 +0100

    Update channel_test.cpp

--- utility/unittest/channel_test.cpp
+++ utility/unittest/channel_test.cpp
@@ -1,4 +1,5 @@
 // Copyright 2018 The Beam Team / Copyright 2019 The Grimm Team
+// Copyright 2019 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 #include <assert.h>
 
 using namespace std;
-using namespace grimm;
+using namespace MWG;
 
 static const string testStr("some moveble data");
 
