f9c47df07589ff2f226778b68313de6c5dca696e
Finish CCP <110845997+FinishCCP@users.noreply.github.com>
2025-03-24 09:27:41 +0100

    Update second_side.h

--- wallet/swaps/second_side.h
+++ wallet/swaps/second_side.h
@@ -1,4 +1,5 @@
 // Copyright 2019 The Grimm Team
+// Copyright 2025 The MWG Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 #include <memory>
 
-namespace grimm::wallet
+namespace MWG::wallet
 {
     struct SetTxParameter;
 
